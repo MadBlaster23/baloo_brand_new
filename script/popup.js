10b77ca933c0b558374f878f004ff94101d6d8d6
evgenii.k <evgenii.k@prodege.com>
2019-03-14 14:43:58 -0700

    JS changed

--- script/popup.js
+++ script/popup.js
@@ -3,14 +3,13 @@ $(function() {
     // contact form animations
     $('#getQuote').click(function() {
         $('#booking').fadeToggle();
-    })
+    });
     $(document).mouseup(function (e) {
         var container = $("#booking");
   
-        if (!container.is(e.target) // if the target of the click isn't the container...
-            && container.has(e.target).length === 0) // ... nor a descendant of the container
-      {
-          container.fadeOut();
-      }
+        if (!container.is(e.target)
+        && container.has(e.target).length === 0) {
+            container.fadeOut();
+        }
     }); 
 });
